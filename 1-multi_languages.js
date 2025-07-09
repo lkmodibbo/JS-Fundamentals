0d323490d8103d9125e13792158f8cd5f960e10e
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-07-09 23:09:49 +0100

    1-multi_anguage file is added

---
+++ 1-multi_languages.js
@@ -0,0 +1,9 @@
+
+const funnyStatememtn = "C is fun"
+console.log(funnyStatememtn);
+
+const truthStatement = "Python is cool"
+console.log(truthStatement);
+
+const amazingStatement = "JavaScript is amazing";
+console.log(amazingStatement);
