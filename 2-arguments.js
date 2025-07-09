1dbcdb855495fe43d5ecdd2ec1e318b7d39c3df0
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-07-09 23:26:42 +0100

    corrected

--- 2-arguments.js
+++ 2-arguments.js
@@ -4,7 +4,7 @@ const NumberOfArguments = process.argv.length - 2;
 if (NumberOfArguments === 0){
     console.log("No argument");
 } else if (NumberOfArguments === 1){
-    console.log("Argument found");
+    console.log("argument found");
 } else {
     console.log("argument found")
 }
