8add70449760c2a8c68f67a5abc0d196496e0dd5
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-07-09 23:25:17 +0100

    argument file added

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+
+const NumberOfArguments = process.argv.length - 2;
+
+if (NumberOfArguments === 0){
+    console.log("No argument");
+} else if (NumberOfArguments === 1){
+    console.log("Argument found");
+} else {
+    console.log("argument found")
+}
