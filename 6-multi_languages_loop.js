1e97d90dddb9f09b66cc5abdac1cd0c724864dea
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-07-10 12:46:37 +0100

    fix typo

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,11 +1,9 @@
+
 const messages = [
     "C is fun",
     "Python is cool",
     "JavaScript is amazing"
 ]
-let outputString = "";
-
 for (const message of messages) {
-    outputString += message + '\n';
+    console.log(message)
 }
-console.log(outputString)
