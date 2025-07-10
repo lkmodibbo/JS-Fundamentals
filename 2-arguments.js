62083171eacb164dcc5172b1d2de73264ac0c8ab
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-07-10 09:10:47 +0100

    fix the typo

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,9 @@
-
 const NumberOfArguments = process.argv.length - 2;
 
-if (NumberOfArguments === 0){
+if (NumberOfArguments === 0) {
     console.log("No argument");
-} else if (NumberOfArguments === 1){
-    console.log("argument found");
-} else {
-    console.log("argument found")
+} else if (NumberOfArguments === 1) {
+    console.log("Argument found");
+} else { 
+    console.log("Arguments found"); 
 }
