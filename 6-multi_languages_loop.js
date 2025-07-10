69ab899e223cc70654cabab75ed6a249f60f91bc
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-07-10 12:33:01 +0100

    for loop is added in a multi_language loops

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+const messages = [
+    "C is fun",
+    "python is cool",
+    "JavaScript is amazig"
+]
+let outputString = "";
+
+for (const message of messages) {
+    outputString += message + '\n';
+}
+console.log(outputString)
